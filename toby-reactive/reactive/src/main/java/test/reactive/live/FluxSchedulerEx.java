c4298bd13cc87f8c329dc70f9cc99e9285a1a005
june <joonie811@naver.com>
2021-06-22 01:06:25 +0900

    flux interval-take operator 구현

--- toby-reactive/reactive/src/main/java/test/reactive/live/FluxSchedulerEx.java
+++ toby-reactive/reactive/src/main/java/test/reactive/live/FluxSchedulerEx.java
@@ -1,17 +1,41 @@
 package test.reactive.live;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
-public class FluxSchedulerEx {
-    public static void main(String[] args) {
-        Flux.range(1, 10)
-                .publishOn(Schedulers.newSingle("pub"))
-                .log()
-                .subscribeOn(Schedulers.newSingle("sub"))
-                .subscribe(System.out::println);
+import java.time.Duration;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-        System.out.println("exit");
+@Slf4j
+public class FluxSchedulerEx {
+    public static void main(String[] args) throws InterruptedException {
+//        Flux.range(1, 10)
+//                .publishOn(Schedulers.newSingle("pub"))
+//                .log()
+//                .subscribeOn(Schedulers.newSingle("sub"))
+//                .subscribe(System.out::println);
+//
+//        System.out.println("exit");
+
+
+        Flux.interval(Duration.ofMillis(200))
+                .take(10)
+                .subscribe(s -> log.debug("onNext:{}", s));
+
+        log.debug("exit");
+        TimeUnit.SECONDS.sleep(5);
+
+
+//        Executors.newSingleThreadExecutor().execute(() -> {
+//            try {
+//                TimeUnit.SECONDS.sleep(2);
+//            } catch (InterruptedException e){}
+//            System.out.println("Hello");
+//        });
+//
+//        System.out.println("exit");
     }
 }
---
+++ toby-reactive/reactive/src/main/java/test/reactive/live/IntervalEx.java
@@ -0,0 +1,97 @@
+package test.reactive.live;
+
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class IntervalEx {
+    public static void main(String[] args) {
+        Publisher<Integer> pub = sub -> {
+            sub.onSubscribe(new Subscription() {
+                int no = 0;
+                volatile boolean cancelled = false;
+
+                @Override
+                public void request(long n) {
+                    ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+                    exec.scheduleAtFixedRate(() -> {
+                        if (cancelled){
+                            exec.shutdown();
+                            return;
+                        }
+                        sub.onNext(no++);
+                    }, 0, 500, TimeUnit.MILLISECONDS);
+                }
+
+                @Override
+                public void cancel() {
+                    cancelled = true;
+                }
+            });
+        };
+
+        Publisher<Integer> takePublisher = new Publisher<Integer>() {
+            @Override
+            public void subscribe(Subscriber<? super Integer> sub) {
+                pub.subscribe(new Subscriber<Integer>() {
+                    int count = 0;
+                    Subscription subscription;
+
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        sub.onSubscribe(s);
+                        this.subscription = s;
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        sub.onNext(integer);
+                        if (++count >= 10){
+                            subscription.cancel();
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        sub.onError(t);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        sub.onComplete();
+                    }
+                });
+            }
+        };
+
+        takePublisher.subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                log.debug("onSubscriber start");
+                s.request(Long.MAX_VALUE);
+                log.debug("onSubscriber end");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                log.debug("onNext: {}", integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                log.debug("onError: {}", t);
+            }
+
+            @Override
+            public void onComplete() {
+                log.debug("onComplete");
+            }
+        });
+    }
+}
