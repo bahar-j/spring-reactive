491076af96f58f71ac935f1af8c4043c6caf2c97
june <joonie811@naver.com>
2021-06-22 16:26:55 +0900

    스프링 비동기 예시

--- toby-reactive/reactive/src/main/java/test/reactive/ReactiveApplication.java
+++ toby-reactive/reactive/src/main/java/test/reactive/ReactiveApplication.java
@@ -1,10 +1,22 @@
 package test.reactive;
 
+import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +25,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 
 @SpringBootApplication
+@Slf4j
+@EnableAsync
 public class ReactiveApplication {
 	@RestController
 	public static class Controller {
@@ -40,8 +56,49 @@ public class ReactiveApplication {
 		}
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReactiveApplication.class, args);
+	@Component
+	public static class MyService {
+		@Async(value = "tp")
+		public ListenableFuture<String> hello() throws InterruptedException {
+			log.info("hello()");
+			Thread.sleep(1000);
+			return new AsyncResult<>("Hello");
+		}
 	}
+
+	@Bean
+	ThreadPoolTaskExecutor tp(){
+		ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
+		te.setCorePoolSize(10);
+		te.setMaxPoolSize(100);
+		te.setQueueCapacity(200);
+		te.setThreadNamePrefix("mythread");
+		te.initialize();
+		return te;
+	};
+
+	public static void main(String[] args) {
+		try (ConfigurableApplicationContext c = SpringApplication.run(ReactiveApplication.class, args)){
+
+		};
+	}
+
+	@Autowired
+	MyService myService;
+
+	// application 뜨면 바로 실행되는 메서드(ApplicationRunner를 리턴하는 메소드)
+	@Bean
+	ApplicationRunner run(){
+		return args -> {
+			log.info("run()");
+			// 비동기일 때는  Future로 받음
+			ListenableFuture<String> f = myService.hello();
+			f.addCallback(success-> System.out.println(success), error -> System.out.println(error.getMessage()));
+			f.cancel(true);
+//			log.info("exit: " + f.isDone());
+//			log.info("result: " + f.get());
+			log.info("exit");
+		};
+	};
 
 }
