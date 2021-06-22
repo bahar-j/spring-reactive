1ff6fa1df2b2c4f710f1b4819e6999a9b43fc107
june <joonie811@naver.com>
2021-06-22 15:08:58 +0900

    비동기 작업 결과물 받아오기(Callback)

--- toby-reactive/reactive/src/main/java/test/reactive/live/FutureEx.java
+++ toby-reactive/reactive/src/main/java/test/reactive/live/FutureEx.java
@@ -2,33 +2,87 @@ package test.reactive.live;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.Objects;
+import java.util.concurrent.*;
 
 @Slf4j
 public class FutureEx {
+
+    interface SuccessCallback {
+        void onSuccess(String result);
+    }
+
+    interface ExceptionCallback {
+        void onError(Throwable t);
+    }
+
+    public static class CallbackFutureTask extends FutureTask<String> {
+        SuccessCallback sc;
+        ExceptionCallback ec;
+
+        // callable: 수행해야할 비동기 작업, sc: 콜백
+        public CallbackFutureTask(Callable<String> callable, SuccessCallback sc, ExceptionCallback ec) {
+            super(callable);
+//            if (sc == null) throw null;
+            this.sc = Objects.requireNonNull(sc);
+            this.ec = Objects.requireNonNull(ec);
+        }
+
+        @Override
+        protected void done() {
+            try {
+                sc.onSuccess(get());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } catch (ExecutionException e) {
+                ec.onError(e.getCause());
+            }
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         // 처음에는 thread 하나도 없는 threadpool. 쓰면서 점차 쌓임
         ExecutorService es = Executors.newCachedThreadPool();
 
-//        es.execute(() -> {
-        Future<String> f = es.submit(() -> {
-//            try {
-                Thread.sleep(2000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
+        CallbackFutureTask f = new CallbackFutureTask(() -> {
+            Thread.sleep(2000);
+            // if (1 == 1) 안하면 밑의 코드가 절대 실행 안되니까 컴파일 에러 뜸
+            if (1 == 1) throw new RuntimeException("Async Error!"); // -> 어쨌든 완료됐으니까 done 호출함
             log.info("Async");
-            return "Hello"; // return 할려면 submit -> Callable 구현
-        }); // Runnable 구현
+            return "Hello";
+        }, new SuccessCallback() {
+            @Override
+            public void onSuccess(String result) {
+                System.out.println(result);
+            }
+        }, new ExceptionCallback() {
+            @Override
+            public void onError(Throwable t) {
+                System.out.println(t.getMessage());
+            }
+        });
 
-        System.out.println(f.isDone());
-        Thread.sleep(2000);
+        // future 구현체 중 하나
+//        FutureTask<String> f = new FutureTask<String>(() -> {
+//            Thread.sleep(2000);
+//            log.info("Async");
+//            return "Hello"; // return 할려면 submit -> Callable 구현
+//        }) { // 익명 클래스
+//            @Override
+//            protected void done() {
+//                // 비동기 작업이 끝나면 호출됨
+//                try {
+//                    System.out.println(get());
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                } catch (ExecutionException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        };
+
+        es.execute(f);
+        es.shutdown();
         log.info("Exit");
-        System.out.println(f.isDone());
-        System.out.println(f.get());
-//        log.info("Exit"); // -> future 쓰면 가장 마지막에 찍힘
     }
 }
